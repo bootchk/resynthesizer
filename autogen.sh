23dced090828c84e2dfd2527f2702f6d3711589d
Lloyd Konneker <bootch@nc.rr.com>
2012-07-31 16:53:19 -0400

    github issue 10

--- autogen.sh
+++ autogen.sh
@@ -68,6 +68,9 @@ elif (automake-1.10 --version) < /dev/null > /dev/null 2>&1; then
 elif (automake-1.11 --version) < /dev/null > /dev/null 2>&1; then
    AUTOMAKE=automake-1.11
    ACLOCAL=aclocal-1.11
+elif (automake-1.12 --version) < /dev/null > /dev/null 2>&1; then
+   AUTOMAKE=automake-1.12
+   ACLOCAL=aclocal-1.12
 elif (automake-1.6 --version) < /dev/null > /dev/null 2>&1; then
    AUTOMAKE=automake-1.6
    ACLOCAL=aclocal-1.6
